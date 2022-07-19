f8f082b64f9aeccc134708f672edfa4642c106c1
DESKTOP-00E21QA\choi3 <wooido22@naver.com>
2022-07-19 15:41:59 +0900

    prj

---
+++ src/main/java/edu/hi/pr/ServletInitializer.java
@@ -0,0 +1,13 @@
+package edu.hi.pr;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootPrj1Application.class);
+	}
+
+}
---
+++ src/main/java/edu/hi/pr/SpringBootPrj1Application.java
@@ -0,0 +1,13 @@
+package edu.hi.pr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPrj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPrj1Application.class, args);
+	}
+
+}
